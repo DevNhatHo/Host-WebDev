9dbbfb2930946fe054019f77f67321acf4d10869
Nhat Ho <nhatcongho90@gmail.com>
2022-02-23 14:48:15 -0800

    Added header, footer, input forms and css.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 
 
+//jshint esversion:6
 //Declarations
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+
+const homeWelcomeMessage = " Welcome!, My name is Nat and I'll be your host today! What is your name and how many is in your party? Also, what is the best number to contact you at ? ";
+const merchantMessage = "Welcome to the merchant page. You can alert and remove customers from here."
 
 const app = express();
 
@@ -12,6 +16,15 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+//uses express function to render home.ejs when diretcory is at localhost:3000
+app.get("/", function(req,res){
+  res.render("home", {startingContent : homeWelcomeMessage});
+})
+
+app.get("/merchant", function(req,res){
+  res.render("merchant", {merchantMessage : merchantMessage});
+})
 
 mongoose.connect("mongodb://localhost:27017/partyDB" , {useNewUrlParser: true});
 
